bc27c0ba6a70c095886a47987c96bfea0642cef2
ShonEagle-X <yashpatel@yudizsolutions.com>
2025-04-03 18:24:12 +0530

    Netcode Entites Setup

---
+++ Assets/DodeBall/Scripts/NetcodePlayerInputSystem.cs
@@ -0,0 +1,48 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+using UnityEngine;
+
+[UpdateInGroup(typeof(GhostInputSystemGroup))]
+partial struct NetcodePlayerInputSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<NetworkStreamInGame>();
+        state.RequireForUpdate<NetcodePlayerInput>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach(RefRW<NetcodePlayerInput> netcodePlayerInput in SystemAPI.Query<RefRW<NetcodePlayerInput>>().WithAll<GhostOwnerIsLocal>())
+        {
+            float2 inputVector = new float2();
+            if(Input.GetKey(KeyCode.W))
+            {
+                inputVector.y += 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                inputVector.y -= 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputVector.x -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputVector.x += 1;
+            }
+            netcodePlayerInput.ValueRW.inputVector = inputVector;
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        
+    }
+}
---
+++ Assets/DodeBall/Scripts/SimpleRpc.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using Unity.NetCode;
+
+public struct SimpleRpc : IRpcCommand
+{
+    public int value;
+}
